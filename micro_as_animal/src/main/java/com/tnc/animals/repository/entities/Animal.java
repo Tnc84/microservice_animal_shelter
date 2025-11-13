11e946e973d1a123f7a762533035df7c85719399
Tnc84 <tancaubogdan@live.com>
2025-11-13 21:45:08 +0200

    feat: add annotation processing profile for shelter module and enhance Animal entity with Lombok annotations and custom equals/hashCode methods

--- micro_as_animal/src/main/java/com/tnc/animals/repository/entities/Animal.java
+++ micro_as_animal/src/main/java/com/tnc/animals/repository/entities/Animal.java
@@ -1,15 +1,20 @@
 package com.tnc.animals.repository.entities;
 
 
-import lombok.Data;
-import org.hibernate.validator.constraints.Length;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Objects;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Animal {
 
     @Id
@@ -37,4 +42,19 @@ public class Animal {
 
     private String photo;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Animal animal = (Animal) o;
+        return getId() != null && Objects.equals(getId(), animal.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
