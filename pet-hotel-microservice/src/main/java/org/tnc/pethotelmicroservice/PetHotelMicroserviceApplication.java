a2dcbc90b8a492aa2544e9565ac26e502dc3a1de
Tnc84 <tancaubogdan@live.com>
2026-01-22 12:24:30 +0200

    add pet hotel microservice

---
+++ pet-hotel-microservice/src/main/java/org/tnc/pethotelmicroservice/PetHotelMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.tnc.pethotelmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHotelMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHotelMicroserviceApplication.class, args);
+    }
+
+}
---
+++ pet-hotel-microservice/src/test/java/org/tnc/pethotelmicroservice/PetHotelMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tnc.pethotelmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHotelMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
